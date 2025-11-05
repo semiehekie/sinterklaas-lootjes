e40ce3b37438210530a8bc5b98e3a33dc1cad02a
semiehekie <152615427+semiehekie@users.noreply.github.com>
2025-11-05 07:47:44 +0100

    Update drawing date in server configuration

--- node/server.js
+++ node/server.js
@@ -10,7 +10,7 @@ const PORT = 5000;
 
 // CONFIGURATIE VARIABELEN - PAS HIER AAN
 const CONFIG = {
-    drawingDate: "2025-10-07T00:00:00", // Datum vanaf wanneer lootjes trekken mogelijk is
+    drawingDate: "2025-11-07T00:00:00", // Datum vanaf wanneer lootjes trekken mogelijk is
     minParticipants: 2, // Minimum aantal deelnemers voordat lootjes trekken mogelijk is
     allowMultipleDraws: false, // Of iemand meerdere keren mag trekken
 };
